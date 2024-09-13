88a23738dc574811fb6cc60014924838281aae8e
Philix27 <philixbob@gmail.com>
2024-09-13 06:43:15 +0100

    Remove nodes

--- src/project.ts
+++ src/project.ts
@@ -1,20 +1,21 @@
 import { makeProject } from "@motion-canvas/core";
-import codec from "./scenes/codec?scene";
 
 import { Code, LezerHighlighter } from "@motion-canvas/2d";
 import { parser } from "@lezer/javascript";
 import welcome from "./scenes/welcome?scene";
-import audio from "./audio/voice.mp3";
+import codec from "./scenes/codec?scene";
+import map from "./scenes/codec?scene";
+// import audio from "./audio/voice.mp3";
 
 Code.defaultHighlighter = new LezerHighlighter(
   parser.configure({
     // Provide a space-separated list of dialects to enable:
-    dialect: "jsx ts tsx",
+    dialect: "jsx ts tsx js",
   })
 );
 
 export default makeProject({
   experimentalFeatures: true,
-  scenes: [welcome, codec],
-  audio: audio,
+  scenes: [welcome, codec, map],
+  // audio: audio,
 });
--- src/scenes/welcome.tsx
+++ src/scenes/welcome.tsx
@@ -1,13 +1,27 @@
-import { Code, Layout, makeScene2D, Node, Txt } from "@motion-canvas/2d";
-import { all, createRef, waitFor, waitUntil } from "@motion-canvas/core";
-import { PlainCode } from "../nodes";
-import { RowItem } from "./comps/Row";
+import {
+  Circle,
+  Code,
+  Layout,
+  makeScene2D,
+  Node,
+  Txt,
+} from "@motion-canvas/2d";
+import {
+  createRef,
+  Direction,
+  slideTransition,
+  waitFor,
+} from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
   const titleText = createRef<Layout>();
+  const smartContractText = createRef<Layout>();
+  const introText = createRef<Txt>();
   const smartContractEth = createRef<Layout>();
   const codeContent = createRef<Node>();
   const code = createRef<Code>();
+  const circle = createRef<Circle>();
+  const circle2 = createRef<Circle>();
 
   view.add(
     <>
@@ -17,9 +31,10 @@ export default makeScene2D(function* (view) {
         gap={20}
         alignItems={"center"}
         direction={"column"}
-        x={-2000}
+        // opacity={0}
+        // x={-2000}
       >
-        <Txt fontSize={100} fill={"white"}>
+        <Txt fontSize={50} fill={"white"} ref={introText}>
           Introduction to Solidity
         </Txt>
       </Layout>
@@ -29,43 +44,42 @@ export default makeScene2D(function* (view) {
         gap={20}
         alignItems={"center"}
         direction={"column"}
-        opacity={0}
-      >
-        <RowItem title="Smart contract" subtitle="Ethereum Blockchain" />
-      </Layout>
-      <Node ref={codeContent} opacity={0}>
-        <PlainCode
-          fontSize={32}
-          fill={"white"}
-          ref={code}
-          code={`
-contract AddressExample {
-    address public myAddress;
-
-    function setMyAddress(address _address) public {
-        myAddress = _address;
-    }
-
-    function getBalance() public view returns (uint) {
-        return myAddress.balance;
-    }
-
-    function transferEther(address payable _to, uint _amount) public {
-        _to.transfer(_amount);
-    }
-
-}  
-`}
-        />
-      </Node>
+        // opacity={0}
+      ></Layout>
+      <Node ref={codeContent}></Node>
+      <Circle opacity={0} ref={circle} x={-400} size={240} fill={"#e13238"} />,
+      <Circle opacity={0} ref={circle2} x={-400} size={240} fill={"#32E1A1"} />,
     </>
   );
 
   yield* waitFor(1);
-  yield* titleText().position.x(-2000, 0.3).to(0, 1);
-  yield* waitUntil("intro");
-  yield* titleText().opacity(0, 0.5);
-  yield* smartContractEth().opacity(1, 0.3).wait(2).to(0, 1);
-  yield* waitUntil("smartContract");
-  yield* codeContent().opacity(1, 0.3);
+  yield* titleText().opacity(1, 0.2).wait(2);
+  // yield* waitUntil("intro");
+  yield* introText().opacity(0, 0.5);
+  yield* smartContractText().opacity(1, 0.5).wait(2);
+  yield* smartContractText().opacity(0, 0.5);
+
+  // yield* smartContractEth().opacity(1, 0.3).wait(2);
+  // yield* waitUntil("sc");
+  // yield* codeContent().opacity(1, useDuration("evn")).wait(3);
+  // yield* code().size(1).opacity(1, 0.3);
+  // yield* waitUntil("codeContent");
+
+  yield* slideTransition(Direction.Left);
+
+  // proceed with the animation
+  yield* waitFor(3);
+
+  // yield* all(
+  //   titleText().opacity(1, 0.2).to(0, 0.5).wait(2),
+  //   smartContractEth().opacity(1, 0.3).to(0, 1).wait(2),
+  //   codeContent().opacity(1, 1).wait(2),
+  //   code().size(1).opacity(1, 0.3),
+  //   spring(PlopSpring, -400, 400, 1, (value) => {
+  //     circle().opacity(1).position.x(value);
+  //   }),
+  //   spring(SmoothSpring, 400, -400, (value) => {
+  //     circle2().opacity(1).position.x(value);
+  //   })
+  // );
 });
